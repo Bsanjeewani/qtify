fe8fab2a84f4a6dd9065e400b167704e0cb13bb4
sanjeewani <sanjeewanibhoite@gmail.com>
2024-04-05 03:11:12 +0530

    final

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,49 +1,19 @@
-import React, { useState } from "react";
-import styles from "./NavBar.module.css";
-import Logo from "../Logo/Logo";
-import SearchBar from "../SearchBar/SearchBar.jsx";
-import FeedBackModal from "../FeedBackModal/FeedBackModal";
-import { useNavigate } from "react-router-dom";
-const NavBar = ({ data, logo = false, search = false, feedback = false }) => {
-  const [isFeedBackModalOpen, setIsFeedBackModalOpen] = useState(false);
-  const navigate = useNavigate();
-  const toggleFeedBackModal = (value = false) => {
-    setIsFeedBackModalOpen(value);
-  };
-  //   const _onSuccess = () => {
-  //     show toast
-  //     showToast("Feedback Submitted", "success");
-  //   };
-  return (
-    <div className={styles.wrapper}>
-      <nav className={styles.navbar}>
-        <div className={styles.logoWrapper} onClick={() => navigate(`/`)}>
-          {logo ? <Logo id={styles.logo} /> : null}
-        </div>
-        {search ? (
-          <div className={styles.searchWrapper}>
-            <SearchBar
-              placeholder="Search a album of your choice"
-              data={data}
-            />
-          </div>
-        ) : null}
-        {feedback ? (
-          <div
-            className={styles.nav_link}
-            onClick={() => toggleFeedBackModal(true)}
-          >
-            Feedback
-          </div>
-        ) : null}
-      </nav>
-      <FeedBackModal
-        isOpen={isFeedBackModalOpen}
-        //    onSuccess={_onSuccess}
-        onDismiss={toggleFeedBackModal}
-      />
-    </div>
-  );
-};
+import React from 'react'
+import "./Navbar.css"
+import Searchbar from '../Searchbar/Searchbar'
+import Feedback from '../FeedbackButton/FeedbackButton'
+import Logo from '../../Images/Logo'
 
-export default NavBar;
+const Navbar = ({ onFeedbackButtonClick }) => {
+    return (
+        <div className='navbar'>
+            <div className='logo'><Logo/></div>
+            <div>
+                <Searchbar />
+            </div>
+            <div className='feedback'><Feedback onFeedbackButtonClick={onFeedbackButtonClick}/></div>
+        </div>
+    )
+}
+
+export default Navbar
